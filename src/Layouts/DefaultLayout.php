4266187ad01b1756b8898a0a6515a9fc13f67da9
James Anderson <jamesanderson9182@users.noreply.github.com>
2017-01-13 23:06:01 +0000

    client collection view

--- src/Layouts/DefaultLayout.php
+++ src/Layouts/DefaultLayout.php
@@ -52,9 +52,7 @@ class DefaultLayout extends BaseLayout
         <a href="/"><img src="/static/images/logo.png" alt="logo" height="55px"></a>
             <ul>
                 <li><h1><a href="/assets/">Assets</a></h1></li>
-                <li><h1><a href="/assets/types/">Assets Types</a></h1></li>
-                <li><h1><a href="/assets/manufacturers/">Manufacturers</a></h1></li>
-                <li><h1><a href="/assets/serials/">Serial Numbers</a></h1></li>
+                <li><h1><a href="/clients/">Clients</a></h1></li>
             </ul>
         </nav>
 HTML;
--- src/Leaves/Clients/ClientsCollectionView.php
+++ src/Leaves/Clients/ClientsCollectionView.php
@@ -2,18 +2,39 @@
 
 namespace HexTechnology\Leaves\Clients;
 
+use HexTechnology\Models\Client;
 use Rhubarb\Leaf\Crud\Leaves\CrudView;
+use Rhubarb\Leaf\Table\Leaves\Table;
 
 class ClientsCollectionView extends CrudView
 {
     protected function createSubLeaves()
     {
-        parent::createSubLeaves();
+        $this->registerSubLeaf(
+           $clients = new Table(Client::all(), 50, "Clients")
+        );
+
+        $clients->columns = [
+            "Display Name" => "<a href='{ClientID}/'>{ClientDisplayName}</a>",
+            "Forename",
+            "Surname",
+            "AddressLine1",
+            "Postcode",
+            "Town",
+            "Mobile",
+            "Telephone",
+            "Email"
+        ];
     }
 
     protected function printViewContent()
     {
-        print "This is the clients collection page!";
+        ?>
+        <h1 class="title">Clients</h1>
+        <div class='collection-table'>
+            <a href='add/' class='button-add'>Add</a>
+        <?php
+        print $this->leaves["Clients"];
     }
 
 }
