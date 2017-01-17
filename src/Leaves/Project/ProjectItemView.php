f7b8b3fd7ded03fd7e45c179ea9851ce8bcda675
James Anderson <jamesanderson9182@users.noreply.github.com>
2017-01-17 19:26:25 +0000

    Project Related Expenses table

--- src/Leaves/Project/ProjectItemView.php
+++ src/Leaves/Project/ProjectItemView.php
@@ -3,17 +3,34 @@
 namespace HexTechnology\Leaves\Project;
 
 use HexTechnology\Layouts\HexTechnologyItemView;
+use HexTechnology\Models\Expense;
+use Rhubarb\Leaf\Table\Leaves\Table;
+use Rhubarb\Stem\Filters\Equals;
 
 class ProjectItemView extends HexTechnologyItemView
 {
     protected function createSubLeaves()
     {
         parent::createSubLeaves();
+
+        $relatedExpenses = Expense::find(new Equals("ProjectID", $this->model->restModel->UniqueIdentifier));
+
         $this->registerSubLeaf(
             "ProjectID",
             "ProjectName",
-            "ClientID"
+            "ClientID",
+            $expenses = new Table($relatedExpenses, 50, "ExpensesTable")
         );
+
+        $expenses->columns =
+            [
+                "" => "<a href='/expenses/{ExpenseID}/'>view</a>",
+                "ExpenseTitle",
+                "NumberOfUnits",
+                "UnitCost",
+                "TotalCharge",
+                "ExpenseType"
+            ];
     }
 
     protected function printInnerContent()
@@ -24,5 +41,8 @@ class ProjectItemView extends HexTechnologyItemView
                 "ProjectName",
                 "ClientID"
             ]);
+        print "<h2>Related Expenses</h2>";
+        print $this->leaves["ExpensesTable"];
     }
 }
+
