1e55053751290470f996457636ae561da8cba48b
James Anderson <jamesanderson9182@users.noreply.github.com>
2017-02-12 20:24:31 +0000

    please dont die I need you

--- src/Leaves/Times/TimesCollectionView.php
+++ src/Leaves/Times/TimesCollectionView.php
@@ -3,26 +3,6 @@
 namespace HexTechnology\Leaves\Times;
 
 use HexTechnology\Layouts\HexTechnologyCollectionTableView;
-<<<<<<< HEAD
-use Rhubarb\Leaf\Table\Leaves\Table;
-
-class TimesCollectionView extends HexTechnologyCollectionTableView
-{
-    protected function createSubLeaves()
-    {
-        parent::createSubLeaves();
-        /** @var Table $table */
-        $table = $this->leaves["Table"];
-        $table->setCollection($this->model->restCollection);
-        $table->columns = [
-            "" => "<a href='{TimeID}/'>view</a>",
-            "Project.ProjectName",
-            "StartTime",
-            "EndTime",
-            "TotalHours"
-        ];
-    }
-=======
 
 class TimesCollectionView extends HexTechnologyCollectionTableView
 {
@@ -36,6 +16,5 @@ class TimesCollectionView extends HexTechnologyCollectionTableView
 			"TotalHours"
 		];
 	}
->>>>>>> 1e28f5fec01a031446c4ab6fe23d33368acb538c
 
 }
--- src/Leaves/Times/TimesItemView.php
+++ src/Leaves/Times/TimesItemView.php
@@ -3,7 +3,6 @@
 namespace HexTechnology\Leaves\Times;
 
 use HexTechnology\Layouts\HexTechnologyItemView;
-<<<<<<< HEAD
 
 class TimesItemView extends HexTechnologyItemView
 {
@@ -28,30 +27,4 @@ class TimesItemView extends HexTechnologyItemView
             ]
         );
     }
-=======
-use Rhubarb\Leaf\Controls\Common\DateTime\Date;
-
-class TimesItemView extends HexTechnologyItemView
-{
-	protected function createSubLeaves()
-	{
-		parent::createSubLeaves();
-		$this->registerSubLeaf(
-			"StartTime",
-			"EndTime",
-			new Date("StartTime")
-		);
-	}
-
-	protected function printInnerContent()
-	{
-		$this->layoutItemsWithContainer( "",
-			[
-				"StartTime",
-				"EndTime"
-			]
-		);
-	}
->>>>>>> 1e28f5fec01a031446c4ab6fe23d33368acb538c
-
 }
